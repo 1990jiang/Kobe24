74026b61bd9d7e98b6bd4d5532b3e7e6bfc70d8b
Tmac <562752212@qq.com>
2016-08-26 11:34:54 +0800

    我创建了一个Person类

---
+++ kobe24/kobe24/MJPerson.h
@@ -0,0 +1,13 @@
+//
+//  MJPerson.h
+//  kobe24
+//
+//  Created by 蒋俊 on 16/8/26.
+//  Copyright © 2016年 蒋俊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MJPerson : NSObject
+
+@end
---
+++ kobe24/kobe24/MJPerson.m
@@ -0,0 +1,13 @@
+//
+//  MJPerson.m
+//  kobe24
+//
+//  Created by 蒋俊 on 16/8/26.
+//  Copyright © 2016年 蒋俊. All rights reserved.
+//
+
+#import "MJPerson.h"
+
+@implementation MJPerson
+
+@end
