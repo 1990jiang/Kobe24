a6c5c85bc9cdfe961a9aa57b914f42c609f6dccb
Tmac <562752212@qq.com>
2018-02-03 13:36:35 +0800

    创建了一个绿色的View

--- kobe24/kobe24/ViewController.m
+++ kobe24/kobe24/ViewController.m
@@ -21,6 +21,13 @@
   UIView *redView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
   redView.backgroundColor = [UIColor redColor];
   [self.view addSubview:redView];
+  
+  //添加一个绿色View
+  UIView *greenView = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+  greenView.backgroundColor = [UIColor greenColor];
+  [self.view addSubview:greenView];
+  
+  
 }
 
 @end
